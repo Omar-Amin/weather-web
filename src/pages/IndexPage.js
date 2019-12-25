6ddfa806035ecfe8162cbbe18ca5d209011f5b21
Omar Amin <omar_75@hotmail.dk>
2019-12-25 21:06:41 +0100

    Test

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './indexpage.css';
+import token from "../data/token";
 
-function IndexPage() {
-    return (
-        <div className="App">
-            <h1>Hej</h1>
-        </div>
-    );
+class IndexPage extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            data: []
+        }
+
+    }
+
+    render() {
+        fetch("https://api.spotify.com/v1/search?q=ken&type=artist", {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(response => response.json()).then((object) => console.log(object))
+
+        return (
+            <div className="App">
+                <h1></h1>
+            </div>
+        );
+    }
+
 }
 
 export default IndexPage;
