1c162847df5d6f283dbf6eb0947b7c8f96d15ada
Omar Amin <omar_75@hotmail.dk>
2019-12-27 12:44:09 +0100

    Fetching data and stores it for a specific city (Copenhagen)

--- src/components/FiveWeather.js
+++ src/components/FiveWeather.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import './FiveWeather.css';
 import Weather from "./Weather";
+import token from "../data/token";
 
 class FiveWeather extends React.Component {
     constructor() {
         super()
 
         this.state = {
-            data: [1, 2, 3, 4, 5]
+            data: [1, 2, 3, 4, 5],
+            items: {}
         }
 
     }
 
-    render() {
+    componentDidMount() {
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q=Copenhagen,DK&appid=" + token)
+            .then(response => { return response.json() })
+            .then(data => this.setState({ items: data }));
+    }
 
+    render() {
+        console.log(this.state.items)
         return (
             <div className="container">
                 <div className="weather-table">
                     {this.state.data.map(item => (
-                        <Weather day={item} />
+                        <Weather key={item} day={item} />
                     ))}
                 </div>
             </div>
