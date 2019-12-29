78150e607474030ffaf13965f8ec055f54410f3b
Omar Amin <omar_75@hotmail.dk>
2019-12-29 13:57:49 +0100

    Small design changes

--- src/components/FiveWeather.js
+++ src/components/FiveWeather.js
@@ -1,101 +1,110 @@
-import React from 'react';
-import './FiveWeather.css';
+import React from "react";
+import "./FiveWeather.css";
 import Weather from "./Weather";
 import token from "../data/token";
-import DetailedWeather from './DetailedWeather';
+import DetailedWeather from "./DetailedWeather";
 
 class FiveWeather extends React.Component {
     constructor() {
-        super()
+        super();
 
         this.state = {
             data: [{ data: [], day: 0 }],
             lastDay: { data: [], day: 0 }, //maybe used later.
             finishSearch: false,
             dwOpened: false
-        }
+        };
 
-        this.insertData = this.insertData.bind(this)
-        this.switchToDetailed = this.switchToDetailed.bind(this)
-        this.switchToWeathers = this.switchToWeathers.bind(this)
+        this.insertData = this.insertData.bind(this);
+        this.switchToDetailed = this.switchToDetailed.bind(this);
+        this.switchToWeathers = this.switchToWeathers.bind(this);
     }
 
     componentDidMount() {
         //fetches data from api (token is the API-key)
-        fetch("https://api.openweathermap.org/data/2.5/forecast?q=Copenhagen,DK&appid=" + token)
-            .then(response => { return response.json() })
-            .then(data => this.insertData(data))
-
+        fetch(
+            "https://api.openweathermap.org/data/2.5/forecast?q=Copenhagen,DK&appid=" +
+                token
+        )
+            .then(response => {
+                return response.json();
+            })
+            .then(data => this.insertData(data));
     }
 
     // store the 5-days weather information inside its own list
     // day 1 = today, day 2 = tomorrow etc.
     insertData(jsonObject) {
-        console.log(jsonObject)
-        const listOfWeathers = jsonObject.list
-        const date1 = new Date()
-        let date2 = new Date()
-        var currentDay = date1.getDay()
-        let tempList = []
-        let cleanedData = []
+        console.log(jsonObject);
+        const listOfWeathers = jsonObject.list;
+        const date1 = new Date();
+        let date2 = new Date();
+        var currentDay = date1.getDay();
+        let tempList = [];
+        let cleanedData = [];
         listOfWeathers.forEach(element => {
-            date2 = new Date(element.dt * 1000)
+            date2 = new Date(element.dt * 1000);
             if (currentDay !== date2.getDay()) {
-                cleanedData.push({ data: tempList, day: currentDay })
-                tempList = []
-                currentDay = date2.getDay()
+                cleanedData.push({ data: tempList, day: currentDay });
+                tempList = [];
+                currentDay = date2.getDay();
             }
-            tempList.push(element)
-
+            tempList.push(element);
         });
 
         this.setState({
             data: cleanedData,
             lastDay: { data: tempList, day: currentDay },
             finishSearch: true
-        })
+        });
     }
     // switch to DetailedWeather when click on a weather
     switchToDetailed(data) {
         this.setState({
             dwOpened: true
-        })
-        console.log(data)
+        });
+        console.log(data);
     }
 
     switchToWeathers() {
         this.setState({
             dwOpened: false
-        })
+        });
     }
 
     render() {
-        const { finishSearch, data } = this.state
+        const { finishSearch, data } = this.state;
 
         return (
             <div>
-                {this.state.dwOpened === true ? <DetailedWeather switchToWeathers={this.switchToWeathers} /> :
+                {this.state.dwOpened === true ? (
+                    <DetailedWeather switchToWeathers={this.switchToWeathers} />
+                ) : (
                     <div className="container">
                         <div className="weather-table">
-                            {finishSearch === true ?
-                                data.map(item => (
-                                    <Weather
-                                        key={item.day}
-                                        day={item.day}
-                                        data={item.data}
-                                        temperature={Math.round(item.data[0].main.temp - 273.15)}
-                                        humidity={item.data[0].main.humidity}
-                                        wind={item.data[0].wind.speed}
-                                        switchToDetailed={this.switchToDetailed} />))
+                            {finishSearch === true
+                                ? data.map(item => (
+                                      <Weather
+                                          key={item.day}
+                                          day={item.day}
+                                          data={item.data}
+                                          temperature={Math.round(
+                                              item.data[0].main.temp - 273.15
+                                          )}
+                                          humidity={item.data[0].main.humidity}
+                                          wind={item.data[0].wind.speed}
+                                          switchToDetailed={
+                                              this.switchToDetailed
+                                          }
+                                      />
+                                  ))
                                 : null}
-
                         </div>
-                    </div>}
+                    </div>
+                )}
             </div>
-
         );
     }
-
 }
 
 export default FiveWeather;
